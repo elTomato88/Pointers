d6940750660eca0f22ac939edcb654faa1d67b17
TomatoMorph <TomatoMorph@DESKTOP-P84S43R>
2025-05-04 13:52:51 +0500

    123

--- DynamicMemory/main.cpp
+++ DynamicMemory/main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 void FillRand(int arr[], const int n);
 void Print(int arr[], const int n);
+int* push_back(int arr[], int& n, const int value);
 
 void main()
 {
@@ -13,24 +14,15 @@ void main()
 
 	FillRand(arr, n);
 	Print(arr, n);
-
+	for (int i = 0; i < n; i++)
+	{
+		cout << *(arr + i) << "\t";
+	}
+	cout << endl;
 	int value;
 	cout << "Enter additional value: "; cin >> value;
-	//1) Создаем буферный массив нужного размера. 
-	int* buffer = new int[n + 1];
-	//2 копируем все содержимое исходного массива в буферный. 
-	for (int i = 0; i < n;i++)
-	{
-		buffer[i] = arr[i];
-	}
-	//3/ Удаляем исходный массив
-	delete[] arr;
-	//4/ Подменяем адрес в указателе Arr адресом нового массива. 
-	arr = buffer;
-	//5 только после всего этого в конце массива появляется элемент, который можно сохранить добавляемое значение. 
-	arr[n] = value;
-	//6 после того как в массив добавился элемент, количество его элементов увеличивается на один
-	n++;
+	
+	arr=push_back(arr, n, value);
 	//7 Значение добавлено, проверяем результат
 	Print(arr, n);
 
@@ -53,4 +45,23 @@ void Print(int arr[], const int n)
 		cout << arr[i] << "\t";
 	}
 	cout << endl;
+}
+int* push_back(int arr[], int& n, const int value)
+{
+	//1) Создаем буферный массив нужного размера. 
+	int* buffer = new int[n + 1];
+	//2 копируем все содержимое исходного массива в буферный. 
+	for (int i = 0; i < n;i++)
+	{
+		buffer[i] = arr[i];
+	}
+	//3/ Удаляем исходный массив
+	delete[] arr;
+	//4/ Подменяем адрес в указателе Arr адресом нового массива. 
+	arr = buffer;
+	//5 только после всего этого в конце массива появляется элемент, который можно сохранить добавляемое значение. 
+	arr[n] = value;
+	//6 после того как в массив добавился элемент, количество его элементов увеличивается на один
+	n++;
+	return arr;
 }
